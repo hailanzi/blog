99cae292487438a70aad7d064fa356cba756e2b8
hailanzi <962164867@qq.com>
2017-11-24 18:35:57 +0800

    新增验证模块

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -1,17 +1,42 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use app\admin\model\Admin as AdminModel;
+use app\admin\validate\Admin as AdminVal;
 class Admin extends Controller
 {
+	
     public function lists()
-    {
-    	
-       return $this->fetch();
+    {	
+    		
+  	$admin= new AdminModel();
+  	$datas=$admin->select();
+    $this->assign('datas',$datas);
+    return $this->fetch();
     }
 	public function add()
 	{
+		if(request()->isPost())
+		{
+			$data['username']=$_POST['username'];
+			$data['password']=$_POST['password'];	
+			$validate=new AdminVal();
+			$result= $validate->scene('add')->check($data);
+			if(!$result)	
+			{
+				$this->error($validate->getError());
+				die;
+			}	
+			$admin= new AdminModel();
+			$admin->save($data);
+			
+			
+		}
+		
 		return $this->fetch();
 	}
+	
+	
 	public function edit()
 	{
 		return $this->fetch();
