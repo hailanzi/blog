c49f0aa94f476b862e584722e698e26fe6a09aaa
小王 <962164867@qq.com>
2017-11-28 22:28:32 +0800

    完善搜索功能

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -5,11 +5,21 @@ class Base extends Controller
 {
 	
 	public function _initialize(){
+		$this->right();
 		$tagsheader=db('tags')->select();
 		$catesheader=db('cate')->select();	
 		$this->assign('tagsheader',$tagsheader);
 		$this->assign('catesheader',$catesheader);
+		
 	} 
+	
+	public function right()
+	{
+		$datasclick=db('article')->order('click desc')->limit(8)->select();
+		$datasstatus=db('article')->where('state','=',1)->order('click desc')->limit(8)->select();
+		$this->assign('datasclick',$datasclick);
+		$this->assign('datasstatus',$datasstatus);
+	}
 	
    
 }
